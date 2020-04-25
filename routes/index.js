a5ebbe451d5e5aa11ab22d4ca109f7aa03bb9083
nextwell <just.nextwell@gmail.com>
2020-04-25 20:41:38 +0300

    fix

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ let express = require("express"),
 
 router.get("/", async (req, res) => {
 
-    res.sendFile(path.join(__dirname + '/client/application/build/index.html'));
+    res.sendFile(path.join(__dirname + '/../client/application/build/index.html'));
 });
 
 module.exports = router;
