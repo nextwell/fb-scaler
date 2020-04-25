5ee2fff25e21a16eec001f638b51d5b7260e779e
nextwell <just.nextwell@gmail.com>
2020-04-25 20:52:39 +0300

    fix

---
+++ client/application/build/precache-manifest.c01f0f35e598ed5085fe8f9771d209ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d58d11676d1bca275981919a74887f7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9d0e7915f64faf178e6",
+    "url": "/static/css/2.a72c35d3.chunk.css"
+  },
+  {
+    "revision": "c381211a05c235a11d8d",
+    "url": "/static/css/main.3a52b81d.chunk.css"
+  },
+  {
+    "revision": "e9d0e7915f64faf178e6",
+    "url": "/static/js/2.86c3f255.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.86c3f255.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c381211a05c235a11d8d",
+    "url": "/static/js/main.7f167ec6.chunk.js"
+  },
+  {
+    "revision": "b00eda10b8f49cf81f85",
+    "url": "/static/js/runtime-main.f371e43a.js"
+  }
+]);
